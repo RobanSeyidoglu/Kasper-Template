acf9c6077a20ff85d7a0de2218ae1a54ecf285ef
RobanSeyidoglu <robanseyidoglu@gmail.com>
2023-06-06 17:52:23 +0300

    start the project & do header & landing

---
+++ kasper.js
@@ -0,0 +1,24 @@
+// Some Functions In The Header
+let toggleMenu = document.querySelector("nav i");
+let menu = document.querySelector("header nav ul");
+let body = document.body;
+if (toggleMenu.style.display !== "none") {
+  toggleMenu.onclick = function () {
+    if (menu.style.display === "flex") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
+  };
+  document.addEventListener("click", function (event) {
+    let targetElement = event.target;
+    let isClickedInsideMenu = toggleMenu.contains(targetElement);
+    if (!isClickedInsideMenu) {
+      menu.style.display = "none";
+    }
+  });
+} else if (toggleMenu.style.display === "none") {
+  menu.style.display = "flex";
+}
+
+// End Header
